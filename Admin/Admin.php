f9e0ae3fbeb2d1df54ebe04fd55822936aea7be3
ryxeuf <ryxeuf@gmail.com>
2015-04-14 00:14:07 +0200

    mode pour le form builder

--- Admin/Admin.php
+++ Admin/Admin.php
@@ -9,8 +9,49 @@
 namespace AT\AdminBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin as BaseAdmin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\FormBuilder;
 
 class Admin extends BaseAdmin
 {
+    protected $mode = null;
 
+    /**
+     * @param FormBuilder $formBuilder
+     */
+    public function defineFormBuilder(FormBuilder $formBuilder)
+    {
+        $mapper = new FormMapper($this->getFormContractor(), $formBuilder, $this);
+
+        if($this->getMode()) {
+            $method = "configure" . ucwords($this->getMode()) . "Fields";
+            if(method_exists($this, $method)) {
+                $this->{$method}($mapper);
+            }
+        } else {
+            $this->configureFormFields($mapper);
+        }
+
+        foreach ($this->getExtensions() as $extension) {
+            $extension->configureFormFields($mapper);
+        }
+
+        $this->attachInlineValidator();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMode()
+    {
+        return $this->mode;
+    }
+
+    /**
+     * @param mixed $mode
+     */
+    public function setMode($mode)
+    {
+        $this->mode = $mode;
+    }
 }
