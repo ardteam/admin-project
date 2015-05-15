85742a47977ee27395de5ab24ed1b34da34141e2
ryxeuf <ryxeuf@gmail.com>
2015-05-15 15:45:47 +0200

    fa-choice type

--- DependencyInjection/ATAdminExtension.php
+++ DependencyInjection/ATAdminExtension.php
@@ -22,8 +22,8 @@ class ATAdminExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('config.yml');
+//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+//        $loader->load('config.yml');
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,73 +19,26 @@ class Configuration extends \Sonata\AdminBundle\DependencyInjection\Configuratio
     {
         $treeBuilder = parent::getConfigTreeBuilder();
 
-        $rootNode = $treeBuilder->root("sonata_admin", 'array');
+        $rootNode = $treeBuilder->root("at_admin", 'array');
         $rootNode
-//            ->fixXmlConfig('option')
-//            ->fixXmlConfig('admin_service')
-//            ->fixXmlConfig('template')
-//            ->fixXmlConfig('extension')
             ->children()
-                ->arrayNode('assets')
-                ->addDefaultsIfNotSet()
+                ->scalarNode('entity_manager')->defaultNull()->end()
+                ->arrayNode('audit')
+                    ->addDefaultsIfNotSet()
                     ->children()
-                        ->arrayNode('stylesheets')
-                            ->defaultValue(array(
-                                'bundles/sonataadmin/vendor/bootstrap/dist/css/bootstrap.min.css',
-//                                'bundles/sonataadmin/vendor/AdminLTE/css/font-awesome.min.css',
-                                'bundles/atadmin/css/font-awesome.min.css',
-                                'bundles/sonataadmin/vendor/AdminLTE/css/ionicons.min.css',
-                                'bundles/sonataadmin/vendor/AdminLTE/css/AdminLTE.css',
-
-                                'bundles/sonatacore/vendor/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
-
-                                'bundles/sonataadmin/vendor/jqueryui/themes/base/jquery-ui.css',
-
-                                'bundles/sonataadmin/vendor/select2/select2.css',
-                                'bundles/sonataadmin/vendor/select2/select2-bootstrap.css',
-
-                                'bundles/sonataadmin/vendor/x-editable/dist/bootstrap3-editable/css/bootstrap-editable.css',
-
-                                'bundles/sonataadmin/css/styles.css',
-                                'bundles/sonataadmin/css/layout.css'
-                            ))
+                        ->booleanNode('force')->defaultTrue()->end()
+                    ->end()
+                ->end()
+                ->arrayNode('templates')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('form')
                             ->prototype('scalar')->end()
-                        ->end()
-                        ->arrayNode('javascripts')
-                            ->defaultValue(array(
-                                'bundles/sonataadmin/vendor/jquery/dist/jquery.min.js',
-                                'bundles/sonataadmin/vendor/jquery.scrollTo/jquery.scrollTo.min.js',
-
-                                'bundles/sonatacore/vendor/moment/min/moment.min.js',
-
-                                'bundles/sonataadmin/vendor/bootstrap/dist/js/bootstrap.min.js',
-
-                                'bundles/sonatacore/vendor/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
-
-                                'bundles/sonataadmin/vendor/jqueryui/ui/minified/jquery-ui.min.js',
-                                'bundles/sonataadmin/vendor/jqueryui/ui/minified/i18n/jquery-ui-i18n.min.js',
-
-                                'bundles/sonataadmin/jquery/jquery.form.js',
-                                'bundles/sonataadmin/jquery/jquery.confirmExit.js',
-
-                                'bundles/sonataadmin/vendor/x-editable/dist/bootstrap3-editable/js/bootstrap-editable.min.js',
-
-                                'bundles/sonataadmin/vendor/select2/select2.min.js',
-
-                                'bundles/sonataadmin/App.js',
-                                'bundles/sonataadmin/Admin.js',
-                            ))
-                            ->prototype('scalar')->end()
+                            ->defaultValue(array('ATAdminBundle:Form:fa_choice.html.twig'))
                         ->end()
                     ->end()
                 ->end()
             ->end()
-
-
-//            ->scalarNode('persist_filters')->defaultValue(false)->end()
-//
-//            ->end()
-//        ->end()
         ;
 
         return $treeBuilder;
